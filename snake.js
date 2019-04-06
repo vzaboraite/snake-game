3577dc8bf75f2ed0e8f1338f8c3dfdaf5c3f2a65
Viktorija Zaboraitė <25870341+vzaboraite@users.noreply.github.com>
2019-04-06 09:44:52 +0100

    Add snake to canvas

--- snake.js
+++ snake.js
@@ -8,3 +8,24 @@ context.fillStyle = CANVAS_BACKGROUND_COLOR;
 context.strokeStyle = CANVAS_BORDER_COLOR;
 context.fillRect(0, 0, canvas.width, canvas.height);
 context.strokeRect(0, 0, canvas.width, canvas.height);
+
+let snake = [
+  { x: 250, y: 250 },
+  { x: 270, y: 250 },
+  { x: 290, y: 250 },
+  { x: 310, y: 250 },
+  { x: 330, y: 250 }
+];
+
+function drawSnakePart(snakePart) {
+  context.fillStyle = "yellow";
+  context.strokeStyle = "black";
+  context.fillRect(snakePart.x, snakePart.y, 20, 20);
+  context.strokeRect(snakePart.x, snakePart.y, 20, 20);
+}
+
+function drawSnake() {
+  snake.forEach(drawSnakePart);
+}
+
+drawSnake();
